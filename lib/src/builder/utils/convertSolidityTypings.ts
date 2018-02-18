a09ec9ffff5e76a786e68a7d7627ba7089ce0ad3
aitrean <williamprado@cmail.carleton.ca>
2018-02-18 02:51:00 -0700

    Push temporry conversion module.

--- lib/src/builder/utils/convertSolidityTypings.ts
+++ lib/src/builder/utils/convertSolidityTypings.ts
@@ -1,33 +1,31 @@
 export const convert  = (typing: string): string => {
-	//TODO account for arbitrary-size uints, bytes, and account for arrays
-	switch (typing) {
-		case('uint256'): 
-			return 'number'
-		case('uint32'): 
-			return 'number'
-		case('uint8'): 
-			return 'number'
-		case('address'):
-			return 'string'
-		case('bytes32'): 
-			return 'string'
-		case('bytes32[]'):
-			return 'string[]'
-		case('bool'):
-			return 'boolean'
-		default:
-			return typing;
-	}
+	//TODO change this function to something less-hacky
+	//TODO account for fixed-point numbers
+	let conversion = ''
+	conversion += isUint(typing);
+	conversion += isBytes(typing);
+	conversion += isBoolean(typing)
+	conversion += isAddress(typing)
+	conversion += isArray(typing)
+	return conversion
 }
 
-const isArray = (input) => {
-	return input.contains('[]')
+const isArray = (input: string) => {
+	return ~input.indexOf('[]') ? '[]' : ''
 }
 
 const isUint = (input) => {
-	return input.contains('uint')
+	return ~input.indexOf('uint') ? 'number' : ''
 }
 
 const isBytes = (input) => {
-	return (input.contains('bytes') && input !== 'bytes1')
+	return ~input.indexOf('bytes') ? 'any' : ''
+}
+
+const isAddress = (input) => {
+	return ~input.indexOf('address') ? 'string' : ''
+}
+
+const isBoolean = (input) => {
+	return ~input.indexOf('bool') ? 'boolean' : ''
 }
